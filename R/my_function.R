7f2abd459a34958f05a6e8e1eb61218edf00487a
Andy Jorg <jorgensenandreas93@gmail.com>
2024-09-04 13:59:51 +0200

    Added function documentation

--- R/my_function.R
+++ R/my_function.R
@@ -8,3 +8,26 @@ divide <- function(x){
 
 
 
+
+
+
+
+#' Function to mask emails
+#'
+#' @param x character string that should be masked
+#'
+#' @description
+#'
+#' `replace_email()` takes an input string and looks for common email structure. If an email is found ,it replaces the email
+#' with `[email]`
+#'
+#' @return string with email masked as `[email]`
+#' @export
+#'
+#' @examples
+#' replace_email("my test string")
+replace_email <- function(x){
+
+  stringr::str_replace_all(x, "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]+\\b", "[email]") # Common email pattern
+
+}
